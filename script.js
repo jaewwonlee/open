27d1f633c44f064940192045147169ca13fa12f1
Jaewon <109273385+jaewwonlee@users.noreply.github.com>
2026-06-09 02:06:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -371,7 +371,7 @@ function applyMeta(meta) {
   const descriptionText = document.getElementById("descriptionText");
   descriptionText.textContent = researchDescription;
 
-  document.title = meta.site_title || "**WELLCOME TO OPEN**";
+  document.title = meta.site_title || "**WELCOME TO OPEN**";
 }
 
 function getTopClassification(data) {
